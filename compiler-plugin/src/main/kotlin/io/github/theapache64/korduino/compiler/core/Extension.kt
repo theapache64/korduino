d1b35a95d62a3499ccc9d3b50c8daa1548a3b344
theapache64 <theapache64@gmail.com>
2025-09-01 21:53:01 +0530

    Add support for multiple generated files in Extension class

--- compiler-plugin/src/main/kotlin/io/github/theapache64/korduino/compiler/core/Extension.kt
+++ compiler-plugin/src/main/kotlin/io/github/theapache64/korduino/compiler/core/Extension.kt
@@ -5,14 +5,15 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.ir.backend.js.utils.nameWithoutExtension
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import java.nio.file.Path
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.createTempFile
 import kotlin.io.path.writeText
 
 class Extension(
-    private val messageCollector: MessageCollector,
-    private val platform: Arg.Mode.Platform
+    private val messageCollector: MessageCollector, private val platform: Arg.Mode.Platform
 ) : IrGenerationExtension {
 
     companion object {
@@ -22,14 +23,16 @@ class Extension(
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
         val visitor = Visitor(platform)
         moduleFragment.accept(visitor, null)
+        val files = mutableListOf<Path>()
+        val fileName = moduleFragment.files[0].nameWithoutExtension
         val cppCode = visitor.generateCode()
-        val file = createTempFile("main", suffix = ".cpp").apply {
+        val file = createTempFile(fileName, suffix = ".cpp").apply {
             writeText(cppCode)
         }
-        val srcDir = Pio.create(listOf(file))
+        files.add(file) // TODO: Manage multiple files
+        val srcDir = Pio.create(files)
         messageCollector.report(
-            CompilerMessageSeverity.INFO,
-            "$CPP_MSG_PREFIX'${srcDir.absolutePathString()}'"
+            CompilerMessageSeverity.INFO, "$CPP_MSG_PREFIX'${srcDir.absolutePathString()}'"
         )
     }
 }
