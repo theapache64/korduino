2ac2292d093e6edb5f0bd42cceb03e0e5b673c77
theapache64 <theapache64@gmail.com>
2025-10-07 08:36:10 +0530

    Add tests for various logical operators in conditions

--- compiler-plugin/src/test/kotlin/io/github/theapache64/korduino/compiler/stdcpp/IfTest.kt
+++ compiler-plugin/src/test/kotlin/io/github/theapache64/korduino/compiler/stdcpp/IfTest.kt
@@ -257,4 +257,194 @@ class IfTest {
         actualOutput.should.equal(expectedOutput)
     }
 
+    @Test
+    fun ifWithDirectCondition(){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                if (1 > 3) {
+                    println("Hello Kotlin!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                if (1 > 3) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @Test
+    fun ifElseIfWithDirectCondition(){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                if (1 > 3) {
+                    println("Hello Kotlin!")
+                } else if (2 < 3) {
+                    println("Hello Kotlin 2!")
+                } else {
+                    println("Hello Kotlin 3!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                if (1 > 3) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                } else if (2 < 3) {
+                    std::cout << "Hello Kotlin 2!" << std::endl;
+                } else {
+                    std::cout << "Hello Kotlin 3!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @Test
+    fun ifElseIfWithMixedCondition(){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                val x = 1 > 3
+                if (x) {
+                    println("Hello Kotlin!")
+                } else if (2 < 3) {
+                    println("Hello Kotlin 2!")
+                } else {
+                    println("Hello Kotlin 3!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                bool x = 1 > 3;
+                if (x) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                } else if (2 < 3) {
+                    std::cout << "Hello Kotlin 2!" << std::endl;
+                } else {
+                    std::cout << "Hello Kotlin 3!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @Test
+    fun ifWithFunctionCallCondition(){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun isTrue(): Boolean {
+                return true
+            }
+            
+            fun main() : Int {
+                if (isTrue()) {
+                    println("Hello Kotlin!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            bool isTrue() {
+                return true;
+            }
+            int main() {
+                if (isTrue()) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @Test
+    fun ifElseIfWithFunctionCallCondition(){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun isTrue(): Boolean {
+                return true
+            }
+            
+            fun main() : Int {
+                if (isTrue()) {
+                    println("Hello Kotlin!")
+                } else if (!isTrue()) {
+                    println("Hello Kotlin 2!")
+                } else {
+                    println("Hello Kotlin 3!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            bool isTrue() {
+                return true;
+            }
+            int main() {
+                if (isTrue()) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                } else if (!isTrue()) {
+                    std::cout << "Hello Kotlin 2!" << std::endl;
+                } else {
+                    std::cout << "Hello Kotlin 3!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
 }
---
+++ compiler-plugin/src/test/kotlin/io/github/theapache64/korduino/compiler/stdcpp/LogicalOperatorTest.kt
@@ -0,0 +1,238 @@
+package io.github.theapache64.korduino.compiler.stdcpp
+
+import com.github.theapache64.expekt.should
+import com.tschuchort.compiletesting.SourceFile
+import io.github.theapache64.korduino.common.Arg
+import io.github.theapache64.korduino.compiler.util.generateAndCompileCppSourceCode
+import io.github.theapache64.korduino.compiler.util.readActualOutput
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import kotlin.test.Test
+
+class LogicalOperatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = ["&&", "||"])
+    fun basic(operator: String) {
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                val x = 1 > 3 $operator 2 < 4
+                if (x) {
+                    println("Hello Kotlin!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                bool x = 1 > 3 $operator 2 < 4;
+                if (x) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["&&", "||"])
+    fun inIf(operator: String) {
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                val a = 1
+                val b = 2
+                if (a > 0 $operator b > 0) {
+                    println("Hello Kotlin!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                int a = 1;
+                int b = 2;
+                if (a > 0 $operator b > 0) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["&&", "||"])
+    fun inElseIf(operator : String){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                val a = 1
+                val b = 2
+                if (a < 0) {
+                    println("Hello Kotlin!")
+                } else if (a > 0 $operator b > 0) {
+                    println("Hello Kotlin 2!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                int a = 1;
+                int b = 2;
+                if (a < 0) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                } else if (a > 0 $operator b > 0) {
+                    std::cout << "Hello Kotlin 2!" << std::endl;
+                }
+                return 0;
+            }
+
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["&&", "||"])
+    fun inBothIfAndElseIf(operator :String){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                val a = 1
+                val b = 2
+                if (a > 0 $operator b > 0) {
+                    println("Hello Kotlin!")
+                } else if (a > 0 $operator b < 0) {
+                    println("Hello Kotlin 2!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                int a = 1;
+                int b = 2;
+                if (a > 0 $operator b > 0) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                } else if (a > 0 $operator b < 0) {
+                    std::cout << "Hello Kotlin 2!" << std::endl;
+                }
+                return 0;
+            }
+
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["&&", "||"])
+    fun withFunction(operator : String){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun isPositive(num: Int): Boolean {
+                return num > 0
+            }
+
+            fun main() : Int {
+                val a = 1
+                val b = 2
+                if (isPositive(a) $operator isPositive(b)) {
+                    println("Hello Kotlin!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            bool isPositive(int num) {
+                return num > 0;
+            }
+            int main() {
+                int a = 1;
+                int b = 2;
+                if (isPositive(a) $operator isPositive(b)) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                }
+                return 0;
+            }
+
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+    @Test
+    fun mixed(){
+        val input = SourceFile.kotlin(
+            "Main.kt",
+            """
+            fun main() : Int {
+                val a = 1
+                val b = 2
+                if (a > 0 && b > 0 || a < 0) {
+                    println("Hello Kotlin!")
+                }
+                return 0
+            }
+        """.trimIndent(),
+        )
+
+        val actualOutput = generateAndCompileCppSourceCode(listOf(input)).readActualOutput(Arg.Platform.Target.STD_CPP)
+
+        val expectedOutput = """
+            #include <iostream>
+            int main() {
+                int a = 1;
+                int b = 2;
+                if (a > 0 && b > 0 || a < 0) {
+                    std::cout << "Hello Kotlin!" << std::endl;
+                }
+                return 0;
+            }
+            
+        """.trimIndent()
+
+        actualOutput.should.equal(expectedOutput)
+    }
+
+
+
+}
