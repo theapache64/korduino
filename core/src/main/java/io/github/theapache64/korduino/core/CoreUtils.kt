7d0afda61f6c563d73ada5473f92e56f1719dfd8
theapache64 <theapache64@gmail.com>
2026-01-02 21:48:54 +0530

    Add overload for analogRead with Pin parameter

--- core/src/main/java/io/github/theapache64/korduino/core/CoreUtils.kt
+++ core/src/main/java/io/github/theapache64/korduino/core/CoreUtils.kt
@@ -13,4 +13,5 @@ external fun delay(timeInMs: Long)
 external fun pinMode(pin: Pin, mode: PinMode)
 external fun pinMode(gpio: Int, mode: PinMode)
 external fun analogRead(gpio: Int) : Int
+external fun analogRead(pin: Pin): Int
 external fun digitalWrite(gpio: Int, value: Digital)
